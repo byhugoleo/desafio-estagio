66d04431e7d01c9dd166ad05734f7f062fe5d91c
Hugo Leonardo <hugoleonardomachado@hotmail.com>
2022-07-13 17:17:54 -0300

    adicionar métodos de validação e regras

--- protected/models/Passageiro.php
+++ protected/models/Passageiro.php
@@ -30,19 +30,66 @@ class Passageiro extends CActiveRecord
 	{
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
+		date_default_timezone_set('America/Sao_Paulo');
 		return array(
-			array('nome, nascimento, email, telefone, status, data_hora_status', 'required'),
-			array('nome', 'length', 'max'=>100),
+			// geral rules
+			array('nome, nascimento, email, telefone', 'required'),
+			array('email, telefone', 'unique'),
+
+			// attribute nome rules.
+			array('nome', 'length', 'min'=>6, 'max'=>100),
+			array('nome', 'validateNome'),
+
+			// attribute email rules.
 			array('email', 'length', 'max'=>255),
+			array('email', 'email'),
+
+			// attribute telefone rules.
 			array('telefone', 'length', 'max'=>16),
-			array('status', 'length', 'max'=>1),
+			array('telefone', 'validateTelefone'),
+
+			// attribute status rules.
+			array('status', 'filter', 'filter'=>function($value) {return $value ? 'A' : 'I';}),
+
+			// attribute status rules.
+			array('data_hora_status', 'default', 'value'=>date('Y-m-d H:i:s')),
+
+			// atribute obs rules.
 			array('obs', 'length', 'max'=>200),
+
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, nome, nascimento, email, telefone, status, data_hora_status, obs', 'safe', 'on'=>'search'),
+			array('nome, nascimento, email, telefone, status, data_hora_status, obs', 'safe', 'on'=>'search'),
 		);
 	}
 
+	public function validateNome($attribute)
+	{
+		$attributeValue = $this->getAttribute($attribute);
+		if (!mb_strlen($attributeValue))
+			return;
+		// TODO: usar expressão regular para verificar se há digitos no nome.
+		$words = explode(' ', $attributeValue);
+		if (count($words) < 2) {
+			$this->addError($attribute, 'Nome deve ter no mínimo duas palavras.');
+			return;
+		}
+		foreach($words as $word) {
+			if (mb_strlen($word) < 3) {
+				$this->addError($attribute, 'Cada palavra de nome deve ter no mínimo 3 caracteres.');
+				return;
+			}
+		}
+	}
+
+	public function validateTelefone($attribute)
+	{
+		$attributeValue = $this->getAttribute($attribute);
+		$regPattern = '/^[+\d]+[-]{1}[\d]{1,2}[-]{1}[\d]{8,9}$/m';
+		if (!preg_match_all($regPattern, $attributeValue))
+			$this->addError($attribute, 'Telefone inválido. Esperado: +99-99-999999999');
+	}
+	
 	/**
 	 * @return array relational rules.
 	 */
--- protected/views/passageiro/_form.php
+++ protected/views/passageiro/_form.php
@@ -2,12 +2,16 @@
 /* @var $this PassageiroController */
 /* @var $model Passageiro */
 /* @var $form CActiveForm */
+/* FIXME: ao atualizar o registro, não está sendo atualizado a propriedade
+data_hora_status.
+*/ 
 ?>
 
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'passageiro-form',
+	'focus'=>array($model, 'nome'),
 	// Please note: When you enable ajax validation, make sure the corresponding
 	// controller action is handling ajax validation correctly.
 	// There is a call to performAjaxValidation() commented in generated controller code.
@@ -27,7 +31,7 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'nascimento'); ?>
-		<?php echo $form->textField($model,'nascimento'); ?>
+		<?php echo CHtml::activeDateField($model,'nascimento'); ?>
 		<?php echo $form->error($model,'nascimento'); ?>
 	</div>
 
@@ -45,19 +49,13 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status',array('size'=>1,'maxlength'=>1)); ?>
+		<?php echo CHtml::activeDropDownList($model,'status',array('I','A'),array('size'=>1,'maxlength'=>1,'disabled'=>$model->isNewRecord?true:false)); ?>
 		<?php echo $form->error($model,'status'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'data_hora_status'); ?>
-		<?php echo $form->textField($model,'data_hora_status'); ?>
-		<?php echo $form->error($model,'data_hora_status'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->labelEx($model,'obs'); ?>
-		<?php echo $form->textField($model,'obs',array('size'=>60,'maxlength'=>200)); ?>
+		<?php echo CHtml::activeTextArea($model,'obs',array('size'=>60,'maxlength'=>200)); ?>
 		<?php echo $form->error($model,'obs'); ?>
 	</div>
 
